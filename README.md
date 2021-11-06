2c741d549b7d00d34f52045cd775debff66a0b84
misaalanshori <isabungsu@gmail.com>
2021-11-06 22:21:01 +0700

    first nextjs site version

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# misaalanshori.github.io
-a page
+## Making my own website
+The plan is to use NextJS to do a static site generation on Github Actions and then deploy it to Github Pages. Also, by setting the Github Action to run on a schedule we can make the website "semi-dynamic" without an actual backend. So far from what I understand, the Github Actions free limitations is actually very generous, so i think its possible to rebuild the site every couple minutes (maybe every 5-15 minutes). Though for now its unnecessary as we don't have any dynamic data.
+
+
+### Things I plan to use
+- NextJS
+- TailwindCSS
+- Github Actions
+- Github Pages
---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+
+function LinkButtons(props) {
+  return (
+    <a href={props.url} target={props.newtab ? "_blank" : ""} rel="noreferrer" className="text-gray-500 inline-block bg-gray-300 hover:bg-gray-600 filter drop-shadow-lg hover:text-gray-200 rounded font-semibold px-2 py-1 mt-2 mr-2 w-auto">
+      {props.text}
+    </a>
+  )
+}
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Welcome!</title>
+        <meta name="description" content="Hello :)" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="px-10 min-h-screen bg-gray-700 py-6 flex flex-col justify-center sm:py-12">
+        <div>
+
+
+          <p className="text-gray-400 text-sm">Hello! I am</p>
+          <p className="text-3xl font-bold mb-1 mt-1 text-gray-300 filter drop-shadow-2xl">Muhammad Isa Al Anshori</p>
+          <p className="text-gray-400 text-sm">Software Engineering Student</p>
+          <LinkButtons url="https://instagram.com/misaalanshori" newtab="1" text="Instagram"/>
+          <LinkButtons url="https://twitter.com/misaalanshori03" newtab="1" text="Twitter"/>
+          <LinkButtons url="https://www.youtube.com/misaalanshori" newtab="1" text="Youtube"/>
+        </div>
+        
+      </div>
+
+
+    </div>
+  )
+}
