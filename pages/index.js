54b2ddd7f387027ebf4a4d78a5e1482ec55c2b95
misaalanshori <isabungsu@gmail.com>
2021-11-09 18:45:22 +0700

    added second page to the main page

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,14 @@ function LinkButtons(props) {
   )
 }
 
+function LinkActionButtons(props) {
+  return (
+    <a href={props.url} target={props.newtab ? "_blank" : ""} rel="noreferrer" className="text-gray-500 inline-block bg-gray-300 hover:bg-gray-600 filter drop-shadow-lg hover:text-gray-200 rounded font-semibold px-2 py-1 mt-2 mr-2 w-full md:w-5/6">
+      {props.text}
+    </a>
+  )
+}
+
 
 export default function Home() {
   return (
@@ -21,7 +29,7 @@ export default function Home() {
       <div className="px-10 min-h-screen bg-gray-700 py-6 flex flex-col justify-center sm:py-12">
         <div className="">
           <p className="text-gray-400 sm:text-xl lg:text-2xl">Hello! I am</p>
-          <p className="text-2xl sm:text-4xl lg:text-7xl font-bold mb-1 mt-1 text-gray-300 filter drop-shadow-2xl">Muhammad Isa Al Anshori</p>
+          <p className="text-2xl sm:text-4xl lg:text-7xl font-bold mb-1 text-gray-300 filter drop-shadow-2xl">Muhammad Isa Al Anshori</p>
           <p className="text-gray-400 sm:text-xl lg:text-2xl">Software Engineering Student</p>
           <LinkButtons url="https://github.com/misaalanshori" newtab="1" text="Github"/>
           <LinkButtons url="https://www.linkedin.com/in/muhammad-isa-al-anshori-876ba5193" newtab="1" text="LinkedIn"/>
@@ -37,12 +45,9 @@ export default function Home() {
             <p className="text-4xl font-bold mb-1 mt-1 text-gray-300 filter drop-shadow-2xl">I love making things</p>
             <p className="text-gray-400 text-xl">From software to hardware</p>
           </div>
-          <div className="flex flex-col justify-center w-full">
-            <LinkButtons url="https://github.com/misaalanshori" newtab="1" text="Github"/>
-            <LinkButtons url="https://www.linkedin.com/in/muhammad-isa-al-anshori-876ba5193" newtab="1" text="LinkedIn"/>
-            <LinkButtons url="https://instagram.com/misaalanshori" newtab="1" text="Instagram"/>
-            <LinkButtons url="https://twitter.com/misaalanshori03" newtab="1" text="Twitter"/>
-            <LinkButtons url="https://www.youtube.com/misaalanshori" newtab="1" text="Youtube"/>
+          <div className="flex flex-col justify-center items-center w-full">
+            <LinkActionButtons url={typeof window === "undefined" ? "" : window.location.origin} text="This Website"/>
+            <LinkActionButtons url="/covid-id" newtab="1" text="Covid-ID"/>
           </div>
           
 
