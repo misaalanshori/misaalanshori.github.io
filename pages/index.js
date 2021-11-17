dc134b662247a330813c3054de56c83364a70b0a
misaalanshori <isabungsu@gmail.com>
2021-11-17 09:52:42 +0700

    added some animations+projects now load from json

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,46 @@
 import Head from 'next/head'
+import TrackVisibility from 'react-on-screen'
+import { useEffect, useState } from 'react'
 
-function LinkButtons(props) {
+function LinkButtons({url, newtab, text, hide, unhideDelay}) {
   return (
-    <a href={props.url} target={props.newtab ? "_blank" : ""} rel="noreferrer" className="text-gray-500 inline-block bg-gray-300 hover:bg-gray-600 filter drop-shadow-lg hover:text-gray-200 rounded font-semibold px-2 py-1 mt-2 mr-2 w-auto">
-      {props.text}
+    <a 
+    href={url} 
+    target={newtab ? "_blank" : ""} 
+    rel="noreferrer" 
+    style={{transitionDelay: unhideDelay + "ms"}} 
+    className={`transition duration-500 ease-in-out transform ${hide ? "-translate-y-32 opacity-0" : ""} text-gray-500 inline-block bg-gray-300 hover:bg-gray-600 filter drop-shadow-lg hover:text-gray-200 rounded font-semibold px-2 py-1 mt-2 mr-2 w-auto`}
+    >
+      {text}
     </a>
   )
 }
 
-function LinkActionButtons(props) {
+function LinkActionButtons({url, action, newtab, text, unhideDelay, isVisible}) {
   return (
-    <a href={props.url} target={props.newtab ? "_blank" : ""} rel="noreferrer" className="text-gray-500 inline-block bg-gray-300 hover:bg-gray-600 filter drop-shadow-lg hover:text-gray-200 rounded font-semibold px-2 py-1 mt-2 mr-2 w-full md:w-5/6">
-      {props.text}
+    <a 
+    href={url} 
+    target={newtab ? "_blank" : ""} 
+    rel="noreferrer" 
+    style={{transitionDelay: unhideDelay + "ms"}} 
+    className={`transition duration-500 ease-in-out transform ${!isVisible ? "-translate-y-32 opacity-0" : ""} text-gray-500 inline-block bg-gray-300 hover:bg-gray-600 filter drop-shadow-lg hover:text-gray-200 rounded font-semibold px-2 py-1 mt-2 mr-2 w-full sm:w-3/4 md:w-5/6 sm:text-lg`}
+    >
+      {text}
     </a>
   )
 }
 
 
-export default function Home() {
+export default function Home({featuredProjects}) {
+  const [hideComponents, setHideComponents] = useState(true)
+  useEffect(
+    () => {
+      setHideComponents(false)
+      return () => {
+        setHideComponents(true)
+      }
+    }
+  )
   return (
     <div>
       <Head>
@@ -26,28 +49,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="px-10 min-h-screen bg-gray-700 py-6 flex flex-col justify-center sm:py-12">
+      <div id="frontpage" className="px-10 min-h-screen bg-gray-700 py-6 flex flex-col justify-center sm:py-12">
         <div className="">
-          <p className="text-gray-400 sm:text-xl lg:text-2xl">Hello! I am</p>
-          <p className="text-2xl sm:text-4xl lg:text-7xl font-bold mb-1 text-gray-300 filter drop-shadow-2xl">Muhammad Isa Al Anshori</p>
-          <p className="text-gray-400 sm:text-xl lg:text-2xl">Software Engineering Student</p>
-          <LinkButtons url="https://github.com/misaalanshori" newtab="1" text="Github"/>
-          <LinkButtons url="https://www.linkedin.com/in/muhammad-isa-al-anshori-876ba5193" newtab="1" text="LinkedIn"/>
-          <LinkButtons url="https://instagram.com/misaalanshori" newtab="1" text="Instagram"/>
-          <LinkButtons url="https://twitter.com/misaalanshori03" newtab="1" text="Twitter"/>
-          <LinkButtons url="https://www.youtube.com/misaalanshori" newtab="1" text="Youtube"/>
+          <p className={`transition duration-1000 ease-in-out delay-100 transform ${hideComponents ? "-translate-y-48 opacity-0" : ""} text-gray-400 sm:text-xl lg:text-2xl`}>Hello! I am</p>
+          <p className={`transition duration-1000 ease-in-out delay-150 transform ${hideComponents ? "-translate-y-48 opacity-0" : ""} text-2xl sm:text-4xl lg:text-7xl font-bold mb-1 text-gray-300 filter drop-shadow-2xl`}>Muhammad Isa Al Anshori</p>
+          <p className={`transition duration-1000 ease-in-out delay-200 transform ${hideComponents ? "-translate-y-48 opacity-0" : ""}  text-gray-400 sm:text-xl lg:text-2xl`}>Software Engineering Student</p>
+          <LinkButtons hide={hideComponents} unhideDelay={200} url="https://github.com/misaalanshori" newtab="1" text="Github"/>
+          <LinkButtons hide={hideComponents} unhideDelay={300} url="https://www.linkedin.com/in/muhammad-isa-al-anshori-876ba5193" newtab="1" text="LinkedIn"/>
+          <LinkButtons hide={hideComponents} unhideDelay={400} url="https://instagram.com/misaalanshori" newtab="1" text="Instagram"/>
+          <LinkButtons hide={hideComponents} unhideDelay={500} url="https://twitter.com/misaalanshori03" newtab="1" text="Twitter"/>
+          <LinkButtons hide={hideComponents} unhideDelay={600} url="https://www.youtube.com/misaalanshori" newtab="1" text="Youtube"/>
         </div>
         
       </div>
-      <div className="px-10 min-h-screen bg-gray-700 py-6 flex flex-col sm:flex-row-reverse justify-center sm:py-12">
+      <div id="myprojects" className="px-10 min-h-screen bg-gray-700 py-6 flex flex-col sm:flex-row-reverse justify-center sm:py-12">
 
           <div className="flex flex-col justify-center text-center sm:text-right w-full">
-            <p className="text-4xl font-bold mb-1 mt-1 text-gray-300 filter drop-shadow-2xl">I love making things</p>
-            <p className="text-gray-400 text-xl">From software to hardware</p>
+            <p className="text-4xl lg:text-6xl font-bold mb-1 mt-1 text-gray-300 filter drop-shadow-2xl">I love making things</p>
+            <p className="text-gray-400 text-xl lg:text-2xl">From software to hardware</p>
           </div>
           <div className="flex flex-col justify-center items-center w-full">
-            <LinkActionButtons url={typeof window === "undefined" ? "" : window.location.origin} text="This Website"/>
-            <LinkActionButtons url="/covid-id" newtab="1" text="Covid-ID"/>
+            <TrackVisibility once>
+              {featuredProjects.map(
+                (element, index) => 
+                <LinkActionButtons 
+                key={element.key}
+                url={element.url}
+                action={""}
+                text={element.title}
+                unhideDelay={index * 50}/>
+                )}
+            </TrackVisibility>
+            
           </div>
           
 
@@ -57,4 +90,21 @@ export default function Home() {
 
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const fs = require("fs")
+
+  // getting featured project
+  let projectsJSON = JSON.parse(fs.readFileSync("data/projects.json"))
+  let featuredProjects = []
+  projectsJSON.featured.forEach(element => {
+    featuredProjects.push(projectsJSON.projects[element])
+  });
+
+  return {
+    props: {
+      featuredProjects
+    }
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     content: ['./pages/**/*.{js,ts,jsx,tsx}','./components/**/*.{js,ts,jsx,tsx}'],
-    safelist: [/bg-[a-z]+-400/, /ring-[a-z]+-500/]
+    safelist: [/bg-[a-z]+-400/, /ring-[a-z]+-500/, /delay-[0-9]+/]
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
